5d552e0d490015d52588266085f2e52d61c30a1a
Gabriel Moura <gmoura96@icloud.com>
2023-12-01 13:11:16 -0300

    Melhora PHPDoc.

--- src/Utalk.php
+++ src/Utalk.php
@@ -2,18 +2,26 @@
 
 namespace Gabrielmoura\LaravelUtalk;
 
+use Gabrielmoura\LaravelUtalk\Endpoints\ActivityLogs;
+use Gabrielmoura\LaravelUtalk\Endpoints\Channel;
+use Gabrielmoura\LaravelUtalk\Endpoints\Chat;
+use Gabrielmoura\LaravelUtalk\Endpoints\Member;
+use Gabrielmoura\LaravelUtalk\Endpoints\Message;
+use Gabrielmoura\LaravelUtalk\Endpoints\OrganizationInvites;
+use Gabrielmoura\LaravelUtalk\Endpoints\Sector;
+use Gabrielmoura\LaravelUtalk\Endpoints\Webhook;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method channel() Channel
- * @method member() Member
- * @method message() Message
- * @method sector() Sector
- * @method webhook() Webhook
- * @method chat() Chat
- * @method activityLog() ActivityLog
- * @method organizationInvites() OrganizationInvites
+ * @method Channel channel() Channel
+ * @method Member member() Member
+ * @method Message message() Message
+ * @method Sector sector() Sector
+ * @method Webhook webhook() Webhook
+ * @method Chat chat() Chat
+ * @method ActivityLogs activityLog() ActivityLog
+ * @method OrganizationInvites organizationInvites() OrganizationInvites
  * @method withToken(string $token)
  * @method refreshToken(string $token = null)
  * @method PendingRequest api()
